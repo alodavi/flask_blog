d535415c0156058171fc443debec0432ed242836
Aloisia Davì <aloisia.davi@gmail.com>
2016-07-18 11:31:31 +0200

    added some comments

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+'''
+CONFIGURATION OPTIONS
+
+Load default config and override config from an environment variable
+
+'''
+
 DATABASE = '/tmp/flaskr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
