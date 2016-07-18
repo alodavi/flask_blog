a746a128e0d2d89e9aa02f072df8d892e72841f8
Aloisia Davì <aloisia.davi@gmail.com>
2016-07-18 11:35:51 +0200

    updated app.py to solve conflicts

--- app.py
+++ app.py
@@ -72,13 +72,3 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('show_entries'))
 
-
-if __name__ == "__main__":
-    app.debug = True
-    
-    host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
-    app.logger.info("Starting flask app on %s:%s", host, port)
-    app.logger.info("Database on: %s", app.config['DATABASE'])
-    
-    app.run(host=host, port=port)
